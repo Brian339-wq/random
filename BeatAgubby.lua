a0698948e9857bac058c373387b476219812479b
Guest <BrianNovokk@gmail.com>
2025-08-20 10:57:34 -0300

    Update BeatAgubby.lua

--- BeatAgubby.lua
+++ BeatAgubby.lua
@@ -41,7 +41,7 @@ end
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "Beat a Gubby",
+   Name = "Beat a Gubby (1.1)",
    LoadingTitle = "Loading...",
    LoadingSubtitle = "",
    ConfigurationSaving = {
